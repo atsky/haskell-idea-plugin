f877bbbfca3d7611e20f4dfc5602f6c479ba5185
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2013-09-14 19:38:06 +0400

    Check for suitable SDK

--- src/org/jetbrains/haskell/module/HaskellModuleBuilder.java
+++ src/org/jetbrains/haskell/module/HaskellModuleBuilder.java
@@ -3,7 +3,9 @@ package org.jetbrains.haskell.module;
 import com.intellij.ide.util.projectWizard.JavaModuleBuilder;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
+import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import org.jetbrains.haskell.sdk.HaskellSdkType;
 
 public final class HaskellModuleBuilder extends JavaModuleBuilder {
 
@@ -15,5 +17,10 @@ public final class HaskellModuleBuilder extends JavaModuleBuilder {
     @Override
     public void setupRootModel(ModifiableRootModel rootModel) throws ConfigurationException {
         super.setupRootModel(rootModel);
+    }
+
+    @Override
+    public boolean isSuitableSdkType(SdkTypeId sdkType) {
+        return sdkType instanceof HaskellSdkType;
     }
 }
