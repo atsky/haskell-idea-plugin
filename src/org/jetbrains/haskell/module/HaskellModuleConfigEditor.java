b200a5f228bc3bc739cb0d2ddd9ee70e934c1185
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2013-09-21 15:28:45 +0400

    Add source path support

--- src/org/jetbrains/haskell/module/HaskellModuleConfigEditor.java
+++ src/org/jetbrains/haskell/module/HaskellModuleConfigEditor.java
@@ -1,6 +1,12 @@
 package org.jetbrains.haskell.module;
 
+import com.intellij.openapi.module.JavaModuleType;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleConfigurationEditor;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ui.configuration.ClasspathEditor;
+import com.intellij.openapi.roots.ui.configuration.ContentEntriesEditor;
 import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationEditorProvider;
 import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationState;
 
@@ -10,7 +16,13 @@ import java.util.List;
 public final class HaskellModuleConfigEditor implements ModuleConfigurationEditorProvider {
 
     public ModuleConfigurationEditor[] createEditors(ModuleConfigurationState state) {
+        ModifiableRootModel rootModel = state.getRootModel();
+        Module module = rootModel.getModule();
+
+        String moduleName = module.getName();
         List<ModuleConfigurationEditor> editors = new ArrayList<ModuleConfigurationEditor>();
+        editors.add(new ContentEntriesEditor(moduleName, state));
+        editors.add(new ClasspathEditor(state));
         editors.add(new OutputEditor(state));
         return editors.toArray(new ModuleConfigurationEditor[editors.size()]);
     }
--- src/org/jetbrains/haskell/parser/DummyHaskellParser.kt
+++ src/org/jetbrains/haskell/parser/DummyHaskellParser.kt
@@ -20,39 +20,22 @@ import org.jetbrains.haskell.util.lisp.SAtom
 import org.jetbrains.haskell.compiler.GHCInterface
 
 
-public class DummyHaskellParser(val p0: IElementType, val p1: PsiBuilder) {
-    val builder: PsiBuilder = p1;
-
-
+public class DummyHaskellParser(p0: IElementType, p1: PsiBuilder) : BaseParser(p0, p1) {
 
 
     public fun parse(): ASTNode {
-        return parseInternal(p0)
+        return parseInternal(root)
     }
 
-    fun done(marker: PsiBuilder.Marker, result: Boolean, elementType: IElementType): Boolean {
-        if (result) {
-            marker.done(elementType);
-        } else {
-            marker.rollbackTo()
-        }
-        return result;
-    }
-
-    fun mark(): PsiBuilder.Marker {
-        return builder.mark()!!
-    }
 
     fun parseInternal(root: IElementType): ASTNode {
         val rootMarker = mark()
 
         while (!builder.eof()) {
-
-
-
-            builder.advanceLexer()
-
-
+            start(HaskellTokenTypes.HASKELL_TOKEN) {
+                builder.advanceLexer()
+                true
+            }
         }
 
         rootMarker.done(root)
