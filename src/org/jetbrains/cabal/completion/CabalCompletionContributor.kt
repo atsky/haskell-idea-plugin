5858c04ec18c776942bf83383c051273e1da6945
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2013-11-08 14:58:34 +0400

    Fast and dirty cabal completion

---
+++ src/org/jetbrains/cabal/completion/CabalCompletionContributor.kt
@@ -0,0 +1,26 @@
+package org.jetbrains.cabal.completion
+
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.psi.PsiElement
+import org.jetbrains.cabal.parser.*
+import java.util.*
+
+
+public open class CabalCompletionContributor() : CompletionContributor() {
+    public override fun fillCompletionVariants(parameters: CompletionParameters?, result: CompletionResultSet?): Unit {
+        if (parameters?.getCompletionType() == CompletionType.BASIC) {
+            val values = ArrayList<String>()
+            values.addAll(pkgDescrFieldDescrs)
+            values.addAll(buildInfo)
+            for (value in values) {
+                result?.addElement(LookupElementBuilder.create(value + ": ")!!)
+            }
+        }
+    }
+
+
+}
---
+++ src/org/jetbrains/cabal/parser/CabalConstructs.kt
@@ -0,0 +1,59 @@
+package org.jetbrains.cabal.parser
+
+// https://github.com/ghc/packages-Cabal/blob/master/Cabal/Distribution/PackageDescription/Parse.hs
+
+public val buildInfo : List<String> = listOf(
+                            "buildable",
+                            "build-tools",
+                            "cpp-options",
+                            "cc-options",
+                            "ld-options",
+                            "pkgconfig-depends",
+                            "frameworks",
+                            "c-sources",
+                            "default-language",
+                            "other-languages",
+                            "default-extensions",
+                            "other-extensions",
+                            "extensions",
+                            "extra-libraries",
+                            "extra-lib-dirs",
+                            "includes",
+                            "install-includes",
+                            "include-dirs",
+                            "hs-source-dirs",
+                            "other-modules",
+                            "ghc-prof-options",
+                            "ghc-shared-options",
+                            "ghc-options",
+                            "hugs-options",
+                            "nhc98-options",
+                            "jhc-options")
+
+
+
+public val pkgDescrFieldDescrs : List<String> = listOf(
+                            "name",
+                            "version",
+                            "disp",
+                            "cabal-version",
+                            "build-type",
+                            "license",
+                            "license-file",
+                            "copyright",
+                            "maintainer",
+                            "build-depends",
+                            "stability",
+                            "homepage",
+                            "package-url",
+                            "bug-reports",
+                            "synopsis",
+                            "description",
+                            "category",
+                            "author",
+                            "tested-with",
+                            "data-files",
+                            "data-dir",
+                            "extra-source-files",
+                            "extra-tmp-files")
+
