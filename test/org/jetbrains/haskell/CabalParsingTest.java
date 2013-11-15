f9584a93f9fe8f42757f0e7fca410d63164f9959
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2013-11-15 18:36:10 +0400

    Add cabal parsing tests

---
+++ test/org/jetbrains/haskell/CabalParsingTest.java
@@ -0,0 +1,25 @@
+package org.jetbrains.haskell;
+
+import com.intellij.testFramework.ParsingTestCase;
+import org.jetbrains.cabal.parser.CaballParserDefinition;
+
+
+public class CabalParsingTest extends ParsingTestCase {
+    static {
+        System.setProperty("idea.platform.prefix", "Idea");
+    }
+
+
+    public CabalParsingTest() {
+        super("parserTests", "cabal", new CaballParserDefinition());
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return ".";
+    }
+
+    public void testSimple() throws Exception { doTest(true); }
+
+
+}
