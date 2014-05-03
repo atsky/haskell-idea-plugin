f3768374c8ae91d6541864c79176437780044640
atsky <kurbatsky@gmail.com>
2014-05-03 14:29:57 +0200

    Fix the mess

--- plugin/src/org/jetbrains/haskell/util/OSUtil.kt
+++ plugin/src/org/jetbrains/haskell/util/OSUtil.kt
@@ -5,32 +5,32 @@ import java.io.File
 /**
  * Created by Евгений on 04.01.14.
  */
-public val OS : OsUtil = OsUtil()
+public val OS: OsUtil = OsUtil()
 
 public class OsUtil() {
     val osName = System.getProperty("os.name")!!.toLowerCase();
 
-    public val isWindows : Boolean = (osName.indexOf("win") >= 0)
+    public val isWindows: Boolean = (osName.indexOf("win") >= 0)
 
-    public val isMac : Boolean = (osName.indexOf("mac") >= 0) || (osName.indexOf("darwin") >= 0);
+    public val isMac: Boolean = (osName.indexOf("mac") >= 0) || (osName.indexOf("darwin") >= 0);
 
-    public fun getCabalBin() : String {
+    public fun getCabalBin(): String {
         return if (isWindows) {
-            System.getenv("AppData") + File.separator + "Roaming" + File.separator + "cabal" + File.separator + "bin" ;
+            joinPath(System.getenv("AppData")!!, "Roaming", "cabal", "bin")
         } else if (isMac) {
-            return System.getProperty("user.home") + "/Library/Haskell/bin";
+            joinPath(System.getProperty("user.home")!!, "Library" + "Haskell", "bin")
         } else {
-            return System.getProperty("user.home") + File.separator + ".cabal";
+            joinPath(System.getProperty("user.home")!!, ".cabal")
         }
     }
 
-    fun getProgramDataFolder(name : String) : String {
+    fun getProgramDataFolder(name: String): String {
         return if (isWindows) {
-           System.getenv("AppData") + File.separator + name;
+            joinPath(System.getenv("AppData")!!, name);
         } else if (isMac) {
-            return System.getProperty("user.home") + "/Library/Application Support/" + name;
+            joinPath(System.getProperty("user.home")!!, "Library", "Application Support", name);
         } else {
-            return System.getProperty("user.home") + File.separator + "." + name;
+            joinPath(System.getProperty("user.home")!!, "." + name);
         }
     }
 
