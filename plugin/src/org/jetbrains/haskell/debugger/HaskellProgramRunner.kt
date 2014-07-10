65f7c7c4ae5bcca022abb8c089054ea24b33f6f7
octomarat <maratx387@gmail.com>
2014-07-10 20:42:04 +0400

    Some implementation have been added to HaskellProgramRunner

--- plugin/src/org/jetbrains/haskell/debugger/HaskellProgramRunner.kt
+++ plugin/src/org/jetbrains/haskell/debugger/HaskellProgramRunner.kt
@@ -7,20 +7,31 @@ import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.configurations.RunProfile
+import org.jetbrains.haskell.run.haskell.HaskellCommandLineState
+import com.intellij.execution.ExecutionResult
 
 /**
  * Created by marat-x on 7/10/14.
  */
 public class HaskellProgramRunner() : GenericProgramRunner<GenericDebuggerRunnerSettings>() {
+    class object {
+        public val HS_PROGRAM_RUNNER_ID: String = "HaskellProgramRunner"
+    }
+
     override fun doExecute(project: Project, state: RunProfileState, contentToReuse: RunContentDescriptor?,
-                           environment: ExecutionEnvironment): RunContentDescriptor? {
-        throw UnsupportedOperationException()
+                           environment: ExecutionEnvironment): RunContentDescriptor?
+    {
+//        var haskellCmdLineState : HaskellCommandLineState = state as HaskellCommandLineState
+//        var executionResult : ExecutionResult = haskellCmdLineState.execute(environment.getExecutor(), this)
+//        haskellCmdLineState.getConsoleBuilder()?.getConsole()?.attachToProcess(executionResult.getProcessHandler())
+        println("Debugger started")
+        
+        return null
     }
-    override fun getRunnerId(): String {
-        throw UnsupportedOperationException()
-    }
+    override fun getRunnerId(): String = HS_PROGRAM_RUNNER_ID
+
     override fun canRun(executorId: String, profile: RunProfile): Boolean {
-        throw UnsupportedOperationException()
+        return true
     }
 
 }
