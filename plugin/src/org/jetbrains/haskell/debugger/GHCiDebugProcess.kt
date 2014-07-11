434f16c670b565aa38b66f81676c007040209fb6
vlpolyansky <vl-polyansky@yandex.ru>
2014-07-11 13:52:15 +0400

    Fixed import of wrong package

--- plugin/src/org/jetbrains/haskell/debugger/GHCiDebugProcess.kt
+++ plugin/src/org/jetbrains/haskell/debugger/GHCiDebugProcess.kt
@@ -2,11 +2,10 @@ package org.jetbrains.haskell.debugger
 
 import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebugSession
-import java.net.ServerSocket
 import com.intellij.execution.ui.ExecutionConsole
-import sun.java2d.loops.ProcessPath.ProcessHandler
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
 import com.intellij.xdebugger.XSourcePosition
+import com.intellij.execution.process.ProcessHandler
 
 /**
  * Created by vlad on 7/10/14.
@@ -16,10 +15,6 @@ public class GHCiDebugProcess(session: XDebugSession,
                               ghciProcess: Process,
                               executionConsole: ExecutionConsole,
                               processHandler: ProcessHandler) : XDebugProcess(session) {
-
-    class object {
-        private val CONNECTION_TIMEOUT = 60000
-    }
 
     private val debugger: ProcessDebugger
 
