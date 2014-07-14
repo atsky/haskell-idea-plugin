047986a02b9e7ed7e70253b6a83dde719ec8589e
octomarat <maratx387@gmail.com>
2014-07-14 14:30:13 +0400

    HaskellLineBreakpointHandler has been added

---
+++ plugin/src/org/jetbrains/haskell/debugger/HaskellLineBreakpointHandler.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.haskell.debugger
+
+import com.intellij.xdebugger.breakpoints.XBreakpointProperties
+import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.intellij.xdebugger.breakpoints.XBreakpointHandler
+import com.intellij.xdebugger.breakpoints.XBreakpointType
+
+public class HaskellLineBreakpointHandler(breakpointTypeClass : Class<out XBreakpointType<XLineBreakpoint<XBreakpointProperties<*>>, *>>,
+                                          val debugProcess: GHCiDebugProcess)
+                                        : XBreakpointHandler<XLineBreakpoint<XBreakpointProperties<*>>>(breakpointTypeClass)
+{
+    override fun registerBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<out Any?>>) {
+        // unimplemented yet
+        throw UnsupportedOperationException()
+    }
+
+    override fun unregisterBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<out Any?>>, isTemporary: Boolean) {
+        // unimplemented yet
+        throw UnsupportedOperationException()
+    }
+}
