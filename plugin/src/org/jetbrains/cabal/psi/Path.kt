9f9429dcd1b3d34682bd279c30f385425391921e
bronti <anya.yaveyn@yandex.ru>
2014-08-05 16:22:45 +0400

    wildcards checking & highlighting added

--- plugin/src/org/jetbrains/cabal/psi/Path.kt
+++ plugin/src/org/jetbrains/cabal/psi/Path.kt
@@ -6,61 +6,61 @@ import org.jetbrains.cabal.psi.Checkable
 import org.jetbrains.cabal.psi.PropertyValue
 import org.jetbrains.cabal.CabalFile
 import java.io.File
+import java.io.FilenameFilter
 
 public open class Path(node: ASTNode) : ASTWrapperPsiElement(node), Checkable, PropertyValue {
 
     public fun isValidPath(): String? {
-        when ((getParent()!! as PropertyField).getPropertyName().toLowerCase()) {
-            "data-dir"           -> return checkDataDir()
-            "hs-source-dirs"     -> return checkFromRootDir()
-            "include-dirs"       -> return checkFromRootDir()
-            "extra-lib-dirs"     -> return checkFromRootDir()
-            "subdir"             -> return null
-            "main-is"            -> return checkMainIs()
-            "extra-doc-files"    -> return checkFromRootFile()
-            "extra-source-files" -> return checkFromRootFile()
-            "extra-tmp-files"    -> return checkFromRootPath()
-            "data-files"         -> return checkDataFiles()
-            "includes"           -> return checkFileFromIncludes()
-            "install-includes"   -> return checkFileFromIncludes()
-            else       -> return "unchecked"
+        when (getParent()) {
+            is DataDirField      -> return checkDataDir()
+            is HSSourceDirsField -> return checkFromRootDir()
+            is IncludeDirsField  -> return checkFromRootDir()
+            is MainFileField     -> return checkMainIs()
+            is LicenseFilesField -> return checkFromRootFile()
+            else -> {
+                when ((getParent() as PropertyField).getPropertyName().toLowerCase()) {
+                    "extra-lib-dirs"     -> return checkFromRootDir()
+                    "subdir"             -> return null
+                    "extra-doc-files"    -> return checkExtraFile()
+                    "extra-source-files" -> return checkExtraFile()
+                    "extra-tmp-files"    -> return checkFromRootPath()
+                    "data-files"         -> return checkDataFile()
+                    "includes"           -> return checkFileFromIncludes()
+                    "install-includes"   -> return checkFileFromIncludes()
+                    "c-sources"          -> return checkFromRootFile()
+                    else -> return "unchecked"
+                }
+            }
         }
+    }
+
+    public fun filterByWildcard(wildcard: File): Array<File>? {
+        val parentDir = wildcard.getParentFile()!!
+        val ext = wildcard.getName().replaceAll("^\\*(\\..+)$", "$1")
+        return parentDir.listFiles(object: FilenameFilter {
+            override public fun accept(dir: File, name: String): Boolean {
+                return (parentDir.equals(dir)) && (name.matches("^[^.]*\\Q${ext}\\E$"))
+            }
+        })
     }
 
     public fun getCabalRootPath(): String = getCabalFile().getCabalRootPath()
 
+    public fun getCabalFile(): CabalFile = (getContainingFile() as CabalFile)
+
     public fun getFile(): File = File(getText())
+
+    public fun isAbsolute(): Boolean = getFile().isAbsolute()
+
+    public fun getFileWithParent(parentFile: File): File = File(parentFile, getText())
+
+    public fun getFileWithParent(parentFile: String): File = getFileWithParent(File(parentFile))
 
     public fun getFileFromRoot(): File {
         val path = getText()
         val res = File(path)
         if (res.isAbsolute()) return res
         return File(getCabalRootPath(), path)
-    }
-
-    public fun getFileWithParent(parentFile: File): File = File(parentFile, getText())
-
-    public fun getFileWithParent(parentFile: String): File = getFileWithParent(File(parentFile))
-
-    public fun getParentBuildSection(): BuildSection? {
-        var parent = getParent()
-        while (parent != null) {
-            if (parent is BuildSection) return (parent as BuildSection)
-            parent = parent!!.getParent()
-        }
-        return null
-    }
-
-    public fun getCabalFile(): CabalFile = (getContainingFile() as CabalFile)
-
-    public fun extensionIs(ext: String): Boolean {
-        return getFile().getName().matches(".+\\.${ext}$")
-    }
-
-    public fun checkDataDir(): String? {
-        if (!getFileFromRoot().isDirectory()) return "there is no such directory"
-        if (File(getText()).isAbsolute()) return "this directory should be absolute"
-        return null
     }
 
     public fun checkFileInDirs(getDirs: () -> List<Path>?): String? {
@@ -75,18 +75,53 @@ public open class Path(node: ASTNode) : ASTWrapperPsiElement(node), Checkable, P
 
     public fun checkFileFromIncludes(): String? = checkFileInDirs({ getParentBuildSection()?.getIncludeDirs() })
 
+    public fun getParentBuildSection(): BuildSection? {
+        var parent = getParent()
+        while (parent != null) {
+            if (parent is BuildSection) return (parent as BuildSection)
+            parent = parent!!.getParent()
+        }
+        return null
+    }
+
+    public fun extensionIs(ext: String): Boolean {
+        return getFile().getName().matches("^.+\\.\\Q${ext}\\E$")
+    }
+
+    public fun isWildcard(): Boolean {
+        return getFile().getName().matches("^\\*\\.(.+)$")
+    }
+
+    public fun checkWildcard(file: File): String? {
+        val res = filterByWildcard(file)?.size
+        if (res == null) return "there is no such directory"
+        if (res == 0)    return "no file matches this wildcard"
+        return null
+    }
+
+    public fun checkDataDir(): String? {
+        if (!getFileFromRoot().isDirectory()) return "there is no such directory"
+        if (isAbsolute()) return "this path should be relative"
+        return null
+    }
+
     public fun checkMainIs(): String? {
         if (!extensionIs("hs") && !extensionIs("lhs")) return "invalid extension"
+        if (isAbsolute()) return "this path should be relative"
         return checkFileInDirs({ getParentBuildSection()?.getHSSourceDirs() })
     }
 
-    public fun checkLicense(): String? {
-        if (getFileFromRoot().isFile()) return null
+    public fun checkDataFile(): String? {
+        val file = getFileWithParent(getCabalFile().getActualDataDir())
+        if (file.isFile()) return null
+        if (isWildcard()) return checkWildcard(file)
         return "there is no such file"
     }
 
-    public fun checkDataFiles(): String? {
-        if (getFileWithParent(getCabalFile().getActualDataDir()).isFile()) return null
+    public fun checkExtraFile(): String? {
+        val file = getFileFromRoot()
+        if (file.isFile()) return null
+        if (isWildcard()) return checkWildcard(file)
         return "there is no such file"
     }
 
