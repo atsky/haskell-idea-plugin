2a736d666ae1fd37b303a48a0b84741be8ca8d15
octomarat <maratx387@gmail.com>
2014-08-13 14:26:32 +0400

    JSONConverter has been refactored (general pattern for all converting methods has been created)

--- plugin/src/org/jetbrains/haskell/debugger/parser/JSONConverter.kt
+++ plugin/src/org/jetbrains/haskell/debugger/parser/JSONConverter.kt
@@ -12,6 +12,8 @@ import java.util.ArrayList
  */
 public class JSONConverter {
     class object {
+        private val WRONG_OUTPUT_MSG = "Wrong JSON output occured while handling command"
+        private val INFO_TAG = "info"
         private val BREAK_LIST_FOR_LINE_INFO = "break list for line"
         private val BREAKS_TAG = "breaks"
         private val BREAK_INDEX_TAG = "index"
@@ -41,109 +43,85 @@ public class JSONConverter {
         private val EVALUATED_MSG = "evaluated"
 
         public fun checkExceptionFromJSON(json: JSONObject): ExceptionResult? {
-            if (WARNING_MSG.equals(json.get("info")) || EXCEPTION_MSG.equals(json.get("info"))) {
-                return ExceptionResult(json.getString("message"))
-            } else {
-                return null
-            }
+            val cases: List<Case<ExceptionResult>> = listOf(
+                Case({ it.equals(WARNING_MSG) || it.equals(EXCEPTION_MSG) },
+                     { ExceptionResult(json.getString("message")) })
+            )
+            return infoSwitchOrNull(json, cases)
         }
 
         public fun breakpointCommandResultFromJSON(json: JSONObject): BreakpointCommandResult? {
-            val info = json.getString("info")
-            if (info.equals(BREAKPOINT_NOT_SET_MSG)) {
-                return null
-            } else if (info.equals(BREAKPOINT_SET_MSG)) {
-                return BreakpointCommandResult(json.getInt("index"), filePositionFromJSON(json.getObject("src_span"))!!)
-            } else {
-                throw RuntimeException("Wrong json output occured while handling SetBreakpointCommand result")
-            }
-        }
-
-        public fun filePositionFromJSON(json: JSONObject): HsFilePosition? {
-            val file = json.get("file")
-            if (file != null) {
-                return HsFilePosition(file as String, json.getInt("startline"), json.getInt("startcol"),
-                        json.getInt("endline"), json.getInt("endcol"))
-            } else {
-                return null
-            }
+            val cases: List<Case<BreakpointCommandResult>> = listOf(
+                Case({ it.equals(BREAKPOINT_SET_MSG) },
+                     { BreakpointCommandResult(json.getInt("index"), filePositionFromJSON(json.getObject("src_span"))!!) }),
+                Case({ it.equals(BREAKPOINT_NOT_SET_MSG) },
+                     { null: BreakpointCommandResult? })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - set breakpoint")
         }
 
         public fun stoppedAtFromJSON(json: JSONObject): HsStackFrameInfo? {
-            val info = json.getString("info")
-            if (info.equals(FINISHED_MSG)) {
-                return null
-            } else if (info.equals(PAUSED_MSG)) {
-                return HsStackFrameInfo(filePositionFromJSON(json.getObject("src_span")),
-                        localBindingListFromJSONArray(json.getArray("vars")).list,
-                        json.getString(STOPPED_AT_FUNC_TAG))
-            } else {
-                throw RuntimeException("Wrong json output occured while handling flow command result")
-            }
+            val cases: List<Case<HsStackFrameInfo>> = listOf(
+                Case({ it.equals(PAUSED_MSG) },
+                     { HsStackFrameInfo(filePositionFromJSON(json.getObject("src_span")),
+                                        localBindingListFromJSONArray(json.getArray("vars")).list,
+                                        json.getString(STOPPED_AT_FUNC_TAG)) }),
+                Case({ it.equals(FINISHED_MSG) },
+                     { null: HsStackFrameInfo? })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - flow command")
         }
 
-        public fun localBindingListFromJSONArray(json: JSONArray): LocalBindingList =
-                LocalBindingList(ArrayList(json.toArray().map {
-                    (variable) ->
-                    with (variable as JSONObject) {
-                        LocalBinding(getString("name"), get("type") as String?, get("value") as String?)
-                    }
-                }))
-
         public fun moveHistResultFromJSON(json: JSONObject): MoveHistResult? {
-            val info = json.getString("info")
-            if (info.equals(BACK_MSG) || info.equals(FORWARD_MSG)) {
-                return MoveHistResult(filePositionFromJSON(json.getObject("src_span")),
-                        localBindingListFromJSONArray(json.getArray("vars")))
-            }
-            throw RuntimeException("Wrong json output occured while handling move hist command result")
+            val cases: List<Case<MoveHistResult>> = listOf(
+                Case({ it.equals(BACK_MSG) || it.equals(FORWARD_MSG) },
+                     { MoveHistResult(filePositionFromJSON(json.getObject("src_span")),
+                                      localBindingListFromJSONArray(json.getArray("vars"))) })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - move through history")
         }
 
         public fun expressionTypeFromJSON(json: JSONObject): ExpressionType {
-            val info = json.getString("info")
-            if (info.equals(EXPRESSION_TYPE_MSG)) {
-                return ExpressionType("<unknown>", json.getString("type"))
-            } else {
-                throw RuntimeException("Wrong JSON output occured while handling expression type command result")
-            }
+            val cases: List<Case<ExpressionType>> = listOf(
+                Case({ it.equals(EXPRESSION_TYPE_MSG) },
+                     { ExpressionType("<unknown>", json.getString("type")) })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - expression type") as ExpressionType
         }
 
         public fun evalResultFromJSON(json: JSONObject): EvalResult {
-            val info = json.getString("info")
-            if (info.equals(EVALUATED_MSG)) {
-                return EvalResult(json.getString("type"), json.getString("value"))
-            } else {
-                throw RuntimeException("Wrong JSON output occured while handling expression type command result")
-            }
+            val cases: List<Case<EvalResult>> = listOf(
+                Case({ it.equals(EVALUATED_MSG) },
+                     { EvalResult(json.getString("type"), json.getString("value")) })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - eval") as EvalResult
+        }
+
+        public fun historyResultFromJSON(json: JSONObject): HistoryResult {
+            val cases: List<Case<HistoryResult>> = listOf(
+                Case({ it.equals(HISTORY_MSG) },
+                     { HistoryResult(ArrayList(json.getArray("history").toArray().map {
+                                    (callInfo) ->
+                                        with (callInfo as JSONObject) {
+                                        HsHistoryFrameInfo(getInt("index"), getString("function"),
+                                                                  filePositionFromJSON(getObject("src_span")))
+                                    }
+                                }), json.get("end_reached") as Boolean) })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - get history") as HistoryResult
         }
 
         public fun breaksListFromJSON(json: JSONObject): BreakInfoList {
-            val info = json.getString("info")
-            if(info.equals(BREAK_LIST_FOR_LINE_INFO)) {
-                val indexSpanArray = json.getArray(BREAKS_TAG)
-                val lambda = {(p: Any?) -> BreakInfo(
-                                           (p as JSONObject).getInt(BREAK_INDEX_TAG),
-                                           filePositionFromJSON((p as JSONObject).getObject(SRC_SPAN_TAG)) as HsFilePosition
-                                           )}
-                return BreakInfoList(ArrayList(indexSpanArray.toArray().map(lambda)))
-            } else {
-                throw RuntimeException("Wrong JSON output occured while handling expression type command result")
-            }
-        }
-
-
-        public fun historyResultFromJSON(json: JSONObject): HistoryResult {
-            val info = json.getString("info")
-            if (info.equals(HISTORY_MSG)) {
-                return HistoryResult(ArrayList(json.getArray("history").toArray().map {
-                    (callInfo) ->
-                    with (callInfo as JSONObject) {
-                        HsHistoryFrameInfo(getInt("index"), getString("function"), filePositionFromJSON(getObject("src_span")))
-                    }
-                }), json.get("end_reached") as Boolean)
-            } else {
-                throw RuntimeException("Wrong JSON output occured while handling expression type command result")
-            }
+            val cases: List<Case<BreakInfoList>> = listOf(
+                Case({ it.equals(BREAK_LIST_FOR_LINE_INFO) },
+                     { val indexSpanArray = json.getArray(BREAKS_TAG)
+                       val lambda = {(p: Any?) -> BreakInfo(
+                                     (p as JSONObject).getInt(BREAK_INDEX_TAG),
+                                     filePositionFromJSON((p as JSONObject).getObject(SRC_SPAN_TAG)) as HsFilePosition
+                                    )}
+                       BreakInfoList(ArrayList(indexSpanArray.toArray().map(lambda))) })
+            )
+            return infoSwitchOrThrow(json, cases, WRONG_OUTPUT_MSG + " - breakpoints list") as BreakInfoList
         }
 
         public fun parseJSONObject(string: String): JSONResult {
@@ -166,5 +144,41 @@ public class JSONConverter {
         private fun JSONObject.getArray(key: String): JSONArray {
             return get(key) as JSONArray
         }
+
+        private fun filePositionFromJSON(json: JSONObject): HsFilePosition? {
+            val file = json.get("file")
+            if (file != null) {
+                return HsFilePosition(file as String, json.getInt("startline"), json.getInt("startcol"),
+                        json.getInt("endline"), json.getInt("endcol"))
+            }
+            return null
+        }
+
+        private fun localBindingListFromJSONArray(json: JSONArray): LocalBindingList =
+                LocalBindingList(ArrayList(json.toArray().map {
+                    (variable) ->
+                    with (variable as JSONObject) {
+                        LocalBinding(getString("name"), get("type") as String?, get("value") as String?)
+                    }
+                }))
+
+        private class Case<R: ParseResult>(val condition: (String) -> Boolean, val action: (JSONObject) -> R?)
+
+        private fun infoSwitch<R: ParseResult>(json: JSONObject,
+                                               cases: List<Case<R>>,
+                                               defaultAction: (JSONObject) -> R?): R? {
+            val info = json.getString(INFO_TAG)
+            val resultCase = cases firstOrNull { it.condition(info) }
+            if(resultCase != null) {
+                return resultCase.action(json)
+            }
+            return defaultAction(json)
+        }
+
+        private fun infoSwitchOrThrow<R: ParseResult>(json: JSONObject, cases: List<Case<R>>, throwMsg: String): R? =
+                infoSwitch(json, cases, { throw RuntimeException(throwMsg) })
+
+        private fun infoSwitchOrNull<R: ParseResult>(json: JSONObject, cases: List<Case<R>>): R? =
+                infoSwitch(json, cases, { null: R? })
     }
 }
