7b936a8d73d1b7655061334e5865b83e184960c5
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2014-11-08 16:26:56 +0100

    Fix build

---
+++ plugin/src/org/jetbrains/cabal/settings/CabalSettingsListenerImpl.java
@@ -0,0 +1,10 @@
+package org.jetbrains.cabal.settings;
+
+import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListenerAdapter;
+
+/**
+ * Created by atsky on 11/8/14.
+ */
+public class CabalSettingsListenerImpl extends ExternalSystemSettingsListenerAdapter<CabalProjectSettings>
+                                       implements CabalSettingsListener {
+}
