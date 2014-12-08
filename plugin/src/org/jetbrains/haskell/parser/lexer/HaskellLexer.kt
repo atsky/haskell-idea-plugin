28560d4d62d99572a30a6aa403cb907886319ff0
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2014-12-08 12:22:35 +0100

    Migrate to Kotlin

---
+++ plugin/src/org/jetbrains/haskell/parser/lexer/HaskellLexer.kt
@@ -0,0 +1,7 @@
+package org.jetbrains.haskell.parser.lexer
+
+import com.intellij.lexer.FlexAdapter
+
+import java.io.Reader
+
+public class HaskellLexer : FlexAdapter(_HaskellLexer(null : Reader?))
