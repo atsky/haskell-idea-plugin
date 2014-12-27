3c08ba3accc2706bd8338aa3e419d740ed3646a5
atsky <kurbatsky@gmail.com>
2014-12-27 15:53:52 +0100

    add recovery test

---
+++ plugin/test/org/jetbrains/haskell/parser/RecoveryTest.java
@@ -0,0 +1,22 @@
+package org.jetbrains.haskell.parser;
+
+import com.intellij.testFramework.ParsingTestCase;
+import org.junit.Test;
+
+public class RecoveryTest extends ParsingTestCase {
+    static {
+        System.setProperty("idea.platform.prefix", "Idea");
+    }
+
+    public RecoveryTest() {
+        super("recoveryTests", "hs", new HaskellParserDefinition());
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "data";
+    }
+
+    public void testRecovery() throws Exception { doTest(true); }
+
+}
