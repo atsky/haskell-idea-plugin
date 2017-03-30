78d0af90536f7d1144b80cd81c52bbafb5074835
Evgeny Kurbatsky <kurbatsky@gmail.com>
2017-03-31 01:37:24 +0200

    Fix build

--- plugin/src/org/jetbrains/yesod/hamlet/parser/HamletTokenTypes.kt
+++ plugin/src/org/jetbrains/yesod/hamlet/parser/HamletTokenTypes.kt
@@ -8,37 +8,80 @@ import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
 
-public interface HamletTokenTypes {
-    companion object {
+object HamletTokenTypes {
+    @JvmField
+    val COMMENT_END: IElementType = HamletToken("-->")
 
-        public val COMMENT_END: IElementType = HamletToken("-->")
-        public val BINDSTATMENT: IElementType = HamletToken("<-")
-        public val COMMENT_START: IElementType = HamletToken("<!--")
-        public val SLASH: IElementType = HamletToken("/")
-        public val DOT: IElementType = HamletToken(".")
-        public val END_INTERPOLATION: IElementType = HamletToken("}")
-        public val EQUAL: IElementType = HamletToken("=")
-        public val OANGLE: IElementType = HamletToken("<")
-        public val CANGLE: IElementType = HamletToken(">")
+    @JvmField
+    val BINDSTATMENT: IElementType = HamletToken("<-")
 
-        public val IDENTIFIER: IElementType = HamletToken("identifier")
-        public val DOT_IDENTIFIER: IElementType = HamletToken("dot_identifier")
-        public val COLON_IDENTIFIER: IElementType = HamletToken("colon_identifier")
-        public val SHARP_IDENTIFIER: IElementType = HamletToken("sharp_identifier")
-        public val OPERATOR: IElementType = HamletToken("operator")
-        public val STRING: IElementType = HamletToken("string")
-        public val ESCAPE: IElementType = HamletToken("escape")
-        public val NEWLINE: IElementType = HamletToken("newline")
-        public val DOCTYPE: IElementType = HamletToken("doctype")
-        public val COMMENT: IElementType = HamletToken("comment")
-        public val INTERPOLATION: IElementType = HamletToken("interpolation")
+    @JvmField
+    val COMMENT_START: IElementType = HamletToken("<!--")
 
-        public val ANY: IElementType = HamletCompositeElementType("ANY")
-        public val TAG: IElementType = HamletCompositeElementType("TAG")
-        public val ATTRIBUTE: IElementType = HamletCompositeElementType("ATTRIBUTE")
-        public val ATTRIBUTE_VALUE: IElementType = HamletCompositeElementType("ATTRIBUTE_VALUE")
+    @JvmField
+    val SLASH: IElementType = HamletToken("/")
 
-        public val WHITESPACES: TokenSet = TokenSet.create(TokenType.WHITE_SPACE)
-    }
+    @JvmField
+    val DOT: IElementType = HamletToken(".")
+
+    @JvmField
+    val END_INTERPOLATION: IElementType = HamletToken("}")
+
+    @JvmField
+    val EQUAL: IElementType = HamletToken("=")
+
+    @JvmField
+    val OANGLE: IElementType = HamletToken("<")
+
+    @JvmField
+    val CANGLE: IElementType = HamletToken(">")
+
+    @JvmField
+    val IDENTIFIER: IElementType = HamletToken("identifier")
+
+    @JvmField
+    val DOT_IDENTIFIER: IElementType = HamletToken("dot_identifier")
+
+    @JvmField
+    val COLON_IDENTIFIER: IElementType = HamletToken("colon_identifier")
+
+    @JvmField
+    val SHARP_IDENTIFIER: IElementType = HamletToken("sharp_identifier")
+
+    @JvmField
+    val OPERATOR: IElementType = HamletToken("operator")
+
+    @JvmField
+    val STRING: IElementType = HamletToken("string")
+
+    @JvmField
+    val ESCAPE: IElementType = HamletToken("escape")
+
+    @JvmField
+    val NEWLINE: IElementType = HamletToken("newline")
+
+    @JvmField
+    val DOCTYPE: IElementType = HamletToken("doctype")
+
+    @JvmField
+    val COMMENT: IElementType = HamletToken("comment")
+
+    @JvmField
+    val INTERPOLATION: IElementType = HamletToken("interpolation")
+
+    @JvmField
+    val ANY: IElementType = HamletCompositeElementType("ANY")
+
+    @JvmField
+    val TAG: IElementType = HamletCompositeElementType("TAG")
+
+    @JvmField
+    val ATTRIBUTE: IElementType = HamletCompositeElementType("ATTRIBUTE")
+
+    @JvmField
+    val ATTRIBUTE_VALUE: IElementType = HamletCompositeElementType("ATTRIBUTE_VALUE")
+
+    @JvmField
+    val WHITESPACES: TokenSet = TokenSet.create(TokenType.WHITE_SPACE)
 
 }
